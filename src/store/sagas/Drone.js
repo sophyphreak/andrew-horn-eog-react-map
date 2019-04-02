801048a0a99f21ce8457ff5925a0f0ab18cbe1fe
Andrew <arhorn@smcm.edu>
2019-04-01 22:49:07 -0500

    broke drone sagas into a separate Drone sagas file

---
+++ src/store/sagas/Drone.js
@@ -0,0 +1,44 @@
+import { takeEvery, call, put, cancel, all } from "redux-saga/effects";
+import API from "../api";
+import * as actions from "../actions";
+
+function* watchFetchDroneData() {
+  const { error, data } = yield call(API.findDroneLocation);
+  if (error) {
+    yield put({ type: actions.API_ERROR, code: error.code });
+    yield cancel();
+    return;
+  }
+  yield put({ type: actions.DRONE_DATA_RECEIVED, data });
+}
+
+function* watchDroneDataReceived(action) {
+  const { latitude, longitude } = action.data.data[0];
+  const { error, data } = yield call(
+    API.findLocationByLatLng,
+    latitude,
+    longitude
+  );
+  if (error) {
+    console.log({ error });
+    yield put({ type: actions.API_ERROR, code: error.code });
+    yield cancel();
+    return;
+  }
+  const location = data[0] ? data[0].woeid : false;
+  if (!location) {
+    yield put({ type: actions.API_ERROR });
+    yield cancel();
+    return;
+  }
+  yield put({ type: actions.WEATHER_ID_RECEIVED, id: location });
+}
+
+function* watchWeatherLoad() {
+  yield all([
+    takeEvery(actions.FETCH_DRONE_DATA, watchFetchDroneData),
+    takeEvery(actions.DRONE_DATA_RECEIVED, watchDroneDataReceived)
+  ]);
+}
+
+export default [watchWeatherLoad];
--- src/store/sagas/Weather.js
+++ src/store/sagas/Weather.js
@@ -52,38 +52,6 @@ function* watchFetchWeather(action) {
   yield put({ type: actions.WEATHER_ID_RECEIVED, id: location });
 }
 
-function* watchFetchDroneData() {
-  const { error, data } = yield call(API.findDroneLocation);
-  if (error) {
-    yield put({ type: actions.API_ERROR, code: error.code });
-    yield cancel();
-    return;
-  }
-  yield put({ type: actions.DRONE_DATA_RECEIVED, data });
-}
-
-function* watchDroneDataReceived(action) {
-  const { latitude, longitude } = action.data.data[0];
-  const { error, data } = yield call(
-    API.findLocationByLatLng,
-    latitude,
-    longitude
-  );
-  if (error) {
-    console.log({ error });
-    yield put({ type: actions.API_ERROR, code: error.code });
-    yield cancel();
-    return;
-  }
-  const location = data[0] ? data[0].woeid : false;
-  if (!location) {
-    yield put({ type: actions.API_ERROR });
-    yield cancel();
-    return;
-  }
-  yield put({ type: actions.WEATHER_ID_RECEIVED, id: location });
-}
-
 function* watchWeatherDataReceived() {
   yield delay(3500);
   yield put({ type: actions.FETCH_DRONE_DATA });
@@ -91,8 +59,6 @@ function* watchWeatherDataReceived() {
 
 function* watchAppLoad() {
   yield all([
-    takeEvery(actions.FETCH_DRONE_DATA, watchFetchDroneData),
-    takeEvery(actions.DRONE_DATA_RECEIVED, watchDroneDataReceived),
     takeEvery(actions.FETCH_WEATHER, watchFetchWeather),
     takeEvery(actions.WEATHER_ID_RECEIVED, watchWeatherIdReceived),
     takeEvery(actions.WEATHER_DATA_RECEIVED, watchWeatherDataReceived)

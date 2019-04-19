61f111a8a1d9102ce3d8cc016905d49c568b6f77
Andrew Horn <arhorn@smcm.edu>
2019-04-19 00:05:33 -0500

    added cors workaround

--- src/store/api/findDroneLocation.js
+++ src/store/api/findDroneLocation.js
@@ -2,7 +2,7 @@ import "isomorphic-fetch";
 
 const findDroneLocation = async () => {
   const response = await fetch(
-    `https://eog-api.andrew-horn-portfolio.life/api/drone`
+    `https://cors-anywhere.herokuapp.com/https://eog-api.andrew-horn-portfolio.life/api/drone`
   );
   if (!response.ok) {
     return { error: { code: response.status } };

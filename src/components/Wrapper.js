fffa671677cb4e7f50bfda897c0187ed3af19b2b
Andrew Horn <arhorn@smcm.edu>
2019-04-25 22:58:51 -0500

    updated height of background color

--- src/components/Wrapper.js
+++ src/components/Wrapper.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import { withStyles } from '@material-ui/core/styles';
+import React from "react";
+import { withStyles } from "@material-ui/core/styles";
 
 const styles = theme => ({
   wrapper: {
     background: theme.palette.background.main,
-    height: '100vh'
+    height: "140vh"
   }
 });
 
